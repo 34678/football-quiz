89b7fa1620d47f15319bfc511f7131ec99e5fd0d
34678 <3383789730@qq.com>
2018-06-10 03:39:55 +0800

    纸屑动画的停止

--- football-quiz/src/components/HelloWorld.vue
+++ football-quiz/src/components/HelloWorld.vue
@@ -8,7 +8,8 @@
     </div>
     <right class="right" id="right"></right>
     <wrong class="wrong" id="wrong"></wrong>
-    <result :correctTotal="correctTotal"></result>
+    <result v-on:draw="draw()" :correctTotal="correctTotal"></result>
+    <lucky-draw></lucky-draw>
     <!-- 测试问题变化 -->
     <!-- <button>clickme</button> -->
   </div>
@@ -19,6 +20,7 @@ import Answer from '../components/answer'
 import Right from '../components/rightmask'
 import Wrong from '../components/wrongmask'
 import Result from '../components/result'
+import luckyDraw from '../components/luckyDraw'
 export default {
   name: 'HelloWorld',
   data () {
@@ -34,19 +36,34 @@ export default {
       questmp:{},
       answer:'',
       /* 答对总题目 */
-      correctTotal:0
+      correctTotal:0,
+      RAFId:undefined,
+      stop:false,
     }
   },
   components: {
     Answer,
     Right,
     Wrong,
-    Result
+    Result,
+    luckyDraw
   },
   mounted() {
     this.__init();
   },
   methods:{
+    draw(){
+      /* 显示抽奖页面 */
+      /* alert('显示抽奖页面') */
+      /* 先隐藏掉动画效果 */
+      var vm = this;
+      window.cancelAnimationFrame(vm.RAFId);
+      vm.RAFId = undefined;
+      vm.stop = true;
+      vm.$('canvas').remove();
+      /* 进入抽奖页面 */
+      vm.$('.luckyDraw')[0].style.display = "block"
+    },
     choose(val){
       // 判断答案是否正确
       var vm = this;
@@ -75,7 +92,9 @@ export default {
       var tmp = ++vm.current;
       if(tmp>3){
         //答题完毕 出结果
-        alert('答题完毕 出结果')
+        /* alert('答题完毕 出结果') */
+         vm.$('.result')[0].style.display = "block";
+         vm.__animation();
       }else {
         // 显示下一道题目
         var res = [];
@@ -90,7 +109,6 @@ export default {
       }
     },
     __init() {
-
       var vm = this;
       //测试是否有点透 目前mask出来没有
      /*  vm.$('.main').click(function(){
@@ -139,6 +157,262 @@ export default {
             console.log(response);
           });
 
+    },
+    /* 纸屑动画 */
+    __animation() {
+      var vm = this;
+      /* 五彩动画逻辑 */
+      var _createClass = (function() {
+        function defineProperties(target, props) {
+          for (var i = 0; i < props.length; i++) {
+            var descriptor = props[i];
+            descriptor.enumerable = descriptor.enumerable || false;
+            descriptor.configurable = true;
+            if ("value" in descriptor) descriptor.writable = true;
+            Object.defineProperty(target, descriptor.key, descriptor);
+          }
+        }
+        return function(Constructor, protoProps, staticProps) {
+          if (protoProps) defineProperties(Constructor.prototype, protoProps);
+          if (staticProps) defineProperties(Constructor, staticProps);
+          return Constructor;
+        };
+      })();
+
+      function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+          throw new TypeError("Cannot call a class as a function");
+        }
+      }
+
+      var Progress = (function() {
+        function Progress() {
+          var param =
+            arguments.length <= 0 || arguments[0] === undefined
+              ? {}
+              : arguments[0];
+
+          _classCallCheck(this, Progress);
+
+          this.timestamp = null;
+          this.duration = param.duration || Progress.CONST.DURATION;
+          this.progress = 0;
+          this.delta = 0;
+          this.progress = 0;
+          this.isLoop = !!param.isLoop;
+
+          this.reset();
+        }
+
+        Progress.prototype.reset = function reset() {
+          this.timestamp = null;
+        };
+
+        Progress.prototype.start = function start(now) {
+          this.timestamp = now;
+        };
+
+        Progress.prototype.tick = function tick(now) {
+          if (this.timestamp) {
+            this.delta = now - this.timestamp;
+            this.progress = Math.min(this.delta / this.duration, 1);
+
+            if (this.progress >= 1 && this.isLoop) {
+              this.start(now);
+            }
+
+            return this.progress;
+          } else {
+            return 0;
+          }
+        };
+
+        _createClass(Progress, null, [
+          {
+            key: "CONST",
+            get: function get() {
+              return {
+                DURATION: 1000
+              };
+            }
+          }
+        ]);
+
+        return Progress;
+      })();
+
+      var Confetti = (function() {
+        function Confetti(param) {
+          _classCallCheck(this, Confetti);
+
+          this.parent = param.elm || document.body;
+          this.canvas = document.createElement("canvas");
+          this.ctx = this.canvas.getContext("2d");
+          this.width = param.width || this.parent.offsetWidth;
+          this.height = param.height || this.parent.offsetHeight;
+          this.length = param.length || Confetti.CONST.PAPER_LENGTH;
+          this.yRange = param.yRange || this.height * 2;
+          this.progress = new Progress({
+            duration: param.duration,
+            isLoop: true
+          });
+          this.rotationRange =
+            typeof param.rotationLength === "number" ? param.rotationRange : 10;
+          this.speedRange =
+            typeof param.speedRange === "number" ? param.speedRange : 10;
+          this.sprites = [];
+
+          this.canvas.style.cssText = [
+            "display: block",
+            "position: absolute",
+            "top: 0",
+            "left: 0",
+            "pointer-events: none"
+          ].join(";");
+
+          this.render = this.render.bind(this);
+
+          this.build();
+
+          this.parent.append(this.canvas);
+          this.progress.start(performance.now());
+
+        vm.RAFId =  requestAnimationFrame(this.render);
+        }
+
+        Confetti.prototype.build = function build() {
+          for (var i = 0; i < this.length; ++i) {
+            var canvas = document.createElement("canvas"),
+              ctx = canvas.getContext("2d");
+
+            canvas.width = Confetti.CONST.SPRITE_WIDTH;
+            canvas.height = Confetti.CONST.SPRITE_HEIGHT;
+
+            canvas.position = {
+              initX: Math.random() * this.width,
+              initY: -canvas.height - Math.random() * this.yRange
+            };
+
+            canvas.rotation =
+              this.rotationRange / 2 - Math.random() * this.rotationRange;
+            canvas.speed =
+              this.speedRange / 2 + Math.random() * (this.speedRange / 2);
+
+            ctx.save();
+            ctx.fillStyle =
+              Confetti.CONST.COLORS[
+                (Math.random() * Confetti.CONST.COLORS.length) | 0
+              ];
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.restore();
+
+            this.sprites.push(canvas);
+          }
+        };
+
+        Confetti.prototype.render = function render(now) {
+          var progress = this.progress.tick(now);
+
+          this.canvas.width = this.width;
+          this.canvas.height = this.height;
+
+          for (var i = 0; i < this.length; ++i) {
+            this.ctx.save();
+            this.ctx.translate(
+              this.sprites[i].position.initX +
+                this.sprites[i].rotation *
+                  Confetti.CONST.ROTATION_RATE *
+                  progress,
+              this.sprites[i].position.initY +
+                progress * (this.height + this.yRange)
+            );
+            this.ctx.rotate(this.sprites[i].rotation);
+            this.ctx.drawImage(
+              this.sprites[i],
+              -Confetti.CONST.SPRITE_WIDTH *
+                Math.abs(
+                  Math.sin(progress * Math.PI * 2 * this.sprites[i].speed)
+                ) /
+                2,
+              -Confetti.CONST.SPRITE_HEIGHT / 2,
+              Confetti.CONST.SPRITE_WIDTH *
+                Math.abs(
+                  Math.sin(progress * Math.PI * 2 * this.sprites[i].speed)
+                ),
+              Confetti.CONST.SPRITE_HEIGHT
+            );
+            this.ctx.restore();
+          }
+
+          
+          if(vm.stop){
+              window.cancelAnimationFrame(vm.RAFId);
+              vm.RAFId = undefined;
+          }else{
+            vm.RAFId =  requestAnimationFrame(this.render);
+          }
+        };
+
+        _createClass(Confetti, null, [
+          {
+            key: "CONST",
+            get: function get() {
+              return {
+                SPRITE_WIDTH: 9,
+                SPRITE_HEIGHT: 16,
+                PAPER_LENGTH: 100,
+                DURATION: 8000,
+                ROTATION_RATE: 50,
+                COLORS: [
+                  "#EF5350",
+                  "#EC407A",
+                  "#AB47BC",
+                  "#7E57C2",
+                  "#5C6BC0",
+                  "#42A5F5",
+                  "#29B6F6",
+                  "#26C6DA",
+                  "#26A69A",
+                  "#66BB6A",
+                  "#9CCC65",
+                  "#D4E157",
+                  "#FFEE58",
+                  "#FFCA28",
+                  "#FFA726",
+                  "#FF7043",
+                  "#8D6E63",
+                  "#BDBDBD",
+                  "#78909C"
+                ]
+              };
+            }
+          }
+        ]);
+
+        return Confetti;
+      })();
+
+      var start =  function() {
+        var DURATION = 8000,
+          LENGTH = 120;
+
+        new Confetti({
+          width: window.innerWidth,
+          height: window.innerHeight,
+          length: LENGTH,
+          duration: DURATION
+        });
+
+        setTimeout(function() {
+          new Confetti({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            length: LENGTH,
+            duration: DURATION
+          });
+        }, DURATION / 2);
+      } ;
+      start();
     }
   },
   watch: {
@@ -210,5 +484,14 @@ export default {
     width: 100%;
     height: 100%;
     background: white;
+     display: none;
+  }
+  .luckyDraw{
+        position: absolute;
+        top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: none;
   }
 </style>
--- football-quiz/src/components/luckyDraw.vue
+++ football-quiz/src/components/luckyDraw.vue
@@ -1,1 +1,29 @@
 /* 抽奖页面 */
+<template>
+  <div class="luckyDraw">
+      <div class="draw">ajhdjhajd</div>
+  </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+  mounted() {
+   
+    this.__init();
+  },
+  methods: {
+    __init(){
+        /* 自动进行抽奖 */
+    },
+  }
+};
+</script>
+<style scoped lang="scss" >
+ .luckyDraw{
+     background: black;
+ }
+</style>

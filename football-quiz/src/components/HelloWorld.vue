562d0ad065e1731e68f23be8dca9826fc12724dc
34678 <3383789730@qq.com>
2018-06-12 02:41:11 +0800

    抽奖模块（转盘逐渐转动到奖品）

--- football-quiz/src/components/HelloWorld.vue
+++ football-quiz/src/components/HelloWorld.vue
@@ -10,42 +10,43 @@
     <wrong class="wrong" id="wrong"></wrong>
     <result v-on:draw="draw()" :correctTotal="correctTotal"></result>
     <lucky-draw></lucky-draw>
-    <guide v-on:back="back()" ></guide>
-    <draw></draw>
+    <guide v-on:back="back()" ></guide> 
     <!-- 测试问题变化 -->
     <!-- <button>clickme</button> -->
   </div>
 </template>
 
 <script>
-import Answer from '../components/answer'
-import Right from '../components/rightmask'
-import Wrong from '../components/wrongmask'
-import Result from '../components/result'
-import luckyDraw from '../components/luckyDraw'
-import Guide from '../components/guide'
-import Draw from '../components/draw'
+import Answer from "../components/answer";
+import Right from "../components/rightmask";
+import Wrong from "../components/wrongmask";
+import Result from "../components/result";
+import luckyDraw from "../components/luckyDraw";
+import Guide from "../components/guide";
 import A from "../../static/images/time/1.png";
 import B from "../../static/images/time/2.png";
 import C from "../../static/images/time/3.png";
 import D from "../../static/images/time/4.png";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      question: ' 2014 年巴西世界杯的吉祥物是什么？',
-      option:[1,2,3,4],
-      images:[A,B,C,D],
-      current:0,
+      question: " 2014 年巴西世界杯的吉祥物是什么？",
+      option: [1, 2, 3, 4],
+      images: [A, B, C, D],
+      current: 0,
       // 存储问题的数据
-      questmp:{},
-      answer:'',
+      questmp: {},
+      answer: "",
       /* 答对总题目 */
-      correctTotal:0,
-      RAFId:undefined,
-      stop:false,
-      openid:'https://docs.qq.com/doc/BqsXiL09UcT82GaYdI0TUkDi3gUSyP3VViow0IQmKC2Cjyb92sN59Q2dfqWC4WIxnx1dNh9Q3'
-    }
+      correctTotal: 0,
+      RAFId: undefined,
+      stop: false,
+      openid:
+        "https://docs.qq.com/doc/BqsXiL09UcT82GaYdI0TUkDi3gUSyP3VViow0IQmKC2Cjyb92sN59Q2dfqWC4WIxnx1dNh9Q3",
+        // 答题id
+        id:0
+    };
   },
   components: {
     Answer,
@@ -53,17 +54,16 @@ export default {
     Wrong,
     Result,
     luckyDraw,
-    Guide ,
-    Draw
+    Guide
   },
   mounted() {
     this.__init();
   },
-  methods:{
-    back(){
-       this.$('.guide')[0].style.display = "none"
+  methods: {
+    back() {
+      this.$(".guide")[0].style.display = "none";
     },
-    draw(){
+    draw() {
       /* 显示抽奖页面 */
       /* alert('显示抽奖页面') */
       /* 先隐藏掉动画效果 */
@@ -71,109 +71,137 @@ export default {
       window.cancelAnimationFrame(vm.RAFId);
       vm.RAFId = undefined;
       vm.stop = true;
-      vm.$('canvas').remove();
+      vm.$("canvas").remove();
       /* 进入抽奖组件 */
-     
+      vm.$router.push({name:"draw",params:{
+        id:vm.id
+      }});
     },
-    choose(val){
+    choose(val) {
       // 判断答案是否正确
       var vm = this;
-      if(val == this.answer){
+      if (val == this.answer) {
         /* alert('正确') */
-        this.$('#right')[0].style.display = "block";
-        setTimeout(function(){
-           vm.$('#right')[0].style.display = "none";
-           /* 红色球的颜色要弄回去 */
-           vm.$refs.answer.biaohei(vm.answer);
-           vm.newQuestion();
-        },200);
+        this.$("#right")[0].style.display = "block";
+        setTimeout(function() {
+          vm.$("#right")[0].style.display = "none";
+          /* 红色球的颜色要弄回去 */
+          vm.$refs.answer.biaohei(vm.answer);
+          vm.newQuestion();
+        }, 200);
         vm.correctTotal++;
-        console.log('答对题目数量',vm.correctTotal)
-      }else{
+        console.log("答对题目数量", vm.correctTotal);
+      } else {
         /* alert('错误') */
-        
-        this.$('#wrong')[0].style.display = "block";
-        setTimeout(function(){
-           vm.$('#wrong')[0].style.display = "none";
-           vm.$refs.answer.biaohei(vm.answer);
-           vm.newQuestion();
-           
-        },200);
+
+        this.$("#wrong")[0].style.display = "block";
+        setTimeout(function() {
+          vm.$("#wrong")[0].style.display = "none";
+          vm.$refs.answer.biaohei(vm.answer);
+          vm.newQuestion();
+        }, 200);
       }
-      this.$refs.answer.biaohong(this.answer,this.current);
- 
+      this.$refs.answer.biaohong(this.answer, this.current);
     },
     /* 请求下一道题目 */
-    newQuestion(){
+    newQuestion() {
       var vm = this;
       var tmp = ++vm.current;
-      if(tmp>3){
+      if (tmp > 3) {
         //答题完毕 出
         /* alert('答题完毕 出结果') */
-         vm.$('.result')[0].style.display = "block";
-         vm.__animation();
-      }else {
+        vm.$(".result")[0].style.display = "block";
+        vm.__animation();
+        //提交分数
+        vm.submitScore();
+      } else {
         // 显示下一道题目
         var res = [];
         res.push(vm.questmp[vm.current].a);
         res.push(vm.questmp[vm.current].b);
         res.push(vm.questmp[vm.current].c);
         res.push(vm.questmp[vm.current].d);
-      
+
         vm.option = res;
         vm.question = vm.questmp[vm.current].title;
         vm.answer = vm.questmp[vm.current].correct;
       }
     },
+    submitScore() {
+      var vm = this;
+      this.$http
+        .get("https://www.ipareto.com/zeissSjb/submitScore", {
+          params: {
+            openid: "oYkYa03gpCorQzWW3GCM-uw10aCo",
+            score: vm.correctTotal
+          }
+        })
+        .then(function(response) {
+          console.log(response);
+          /* !! */
+          response.data.msg = "ok";
+          if (response.data.msg == "ok") {
+            /* !! */
+             /* vm.id = response.data.data.id; */
+             vm.id =  "715282859960308";
+           /*  console.log(response.data); */
+          } else {
+            alert("请求问题数据错误");
+          }
+        })
+        .catch(function(response) {
+          //错误处理 比如出现一个蒙层显示网络错误
+          console.log(response);
+           vm.id =  "715282859960308";
+        });
+    },
     __init() {
       var vm = this;
       //测试是否有点透 目前mask出来没有
-     /*  vm.$('.main').click(function(){
+      /*  vm.$('.main').click(function(){
         console.log(1);
       }) */
       /* document.getElementsByTagName('button')[0].addEventListener('touchend',function(){
           vm.question = "大力神杯是由谁颁给世界杯冠军？";
       }) */
       // 初始化问题的位置
-      var ele = document.getElementById('ques');
-      var height = -20 - ele.clientHeight/2;
-      var width = 0 - ele.clientWidth/2;
-      
-      ele.style.marginLeft = width + 'px';
-      ele.style.marginTop = height + 'px';
+      var ele = document.getElementById("ques");
+      var height = -20 - ele.clientHeight / 2;
+      var width = 0 - ele.clientWidth / 2;
+
+      ele.style.marginLeft = width + "px";
+      ele.style.marginTop = height + "px";
       //获取问题数据
-        this.$http.get('https://www.ipareto.com/zeissSjb/getTimus', {
-          params: {
-          }
-          })
-          .then(function (response) {
-            console.log(response);
-            if(response.data.msg =='ok'){
-              vm.questmp = response.data.data;
-              if(vm.current > 3){
-                // 显示蔡司题
+      this.$http
+        .get("https://www.ipareto.com/zeissSjb/getTimus", {
+          params: {}
+        })
+        .then(function(response) {
+          console.log(response);
+          if (response.data.msg == "ok") {
+            vm.questmp = response.data.data;
+            if (vm.current > 3) {
+              // 显示蔡司题
+            } else {
+              // 按顺序显示题目
+              var res = [];
+              res.push(vm.questmp[vm.current].a);
+              res.push(vm.questmp[vm.current].b);
+              res.push(vm.questmp[vm.current].c);
+              res.push(vm.questmp[vm.current].d);
 
-              }else{
-                // 按顺序显示题目
-                var res = [];
-                res.push(vm.questmp[vm.current].a);
-                res.push(vm.questmp[vm.current].b);
-                res.push(vm.questmp[vm.current].c);
-                res.push(vm.questmp[vm.current].d);
-              
-                vm.option = res;
-                vm.question = vm.questmp[vm.current].title;
-                vm.answer = vm.questmp[vm.current].correct;
-              }
-            }else{
-              alert('请求问题数据错误')
+              vm.option = res;
+              vm.question = vm.questmp[vm.current].title;
+              vm.answer = vm.questmp[vm.current].correct;
             }
-          })
-          .catch(function (response) {
-            //错误处理 比如出现一个蒙层显示网络错误
-            console.log(response);
-          });
-
+          } else {
+            alert("请求问题数据错误");
+          }
+        })
+        .catch(function(response) {
+          //错误处理 比如出现一个蒙层显示网络错误
+          console.log(response);
+        });
     },
     /* 纸屑动画 */
     __animation() {
@@ -294,7 +322,7 @@ export default {
           this.parent.append(this.canvas);
           this.progress.start(performance.now());
 
-        vm.RAFId =  requestAnimationFrame(this.render);
+          vm.RAFId = requestAnimationFrame(this.render);
         }
 
         Confetti.prototype.build = function build() {
@@ -361,12 +389,11 @@ export default {
             this.ctx.restore();
           }
 
-          
-          if(vm.stop){
-              window.cancelAnimationFrame(vm.RAFId);
-              vm.RAFId = undefined;
-          }else{
-            vm.RAFId =  requestAnimationFrame(this.render);
+          if (vm.stop) {
+            window.cancelAnimationFrame(vm.RAFId);
+            vm.RAFId = undefined;
+          } else {
+            vm.RAFId = requestAnimationFrame(this.render);
           }
         };
 
@@ -409,7 +436,7 @@ export default {
         return Confetti;
       })();
 
-      var start =  function() {
+      var start = function() {
         var DURATION = 8000,
           LENGTH = 120;
 
@@ -428,105 +455,97 @@ export default {
             duration: DURATION
           });
         }, DURATION / 2);
-      } ;
+      };
       start();
     }
   },
   watch: {
-    question:function(val){
-      var ele = document.getElementById('ques');
-      var height = -20 - ele.clientHeight/2;
-      var width = 0 - ele.clientWidth/2;
-      
-      ele.style.marginLeft = width + 'px';
-      ele.style.marginTop = height + 'px';
+    question: function(val) {
+      var ele = document.getElementById("ques");
+      var height = -20 - ele.clientHeight / 2;
+      var width = 0 - ele.clientWidth / 2;
+
+      ele.style.marginLeft = width + "px";
+      ele.style.marginTop = height + "px";
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
- .wrapper,.main{
-   height: 100%;
-    width: 100%;
- }
- .ques{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    color: white;
-    width: 55%;
- 
-    font-size: 16pt;
-    line-height: 16pt;
-    text-align: center;
- }
+.wrapper,
+.main {
+  height: 100%;
+  width: 100%;
+}
+.ques {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  color: white;
+  width: 55%;
+
+  font-size: 16pt;
+  line-height: 16pt;
+  text-align: center;
+}
 </style>
 <style scoped lang="scss" >
-  #time{ 
-    img{
-      width:rem(165);
-      height: rem(50);
-      /* @include dpr(width, 36px);
+#time {
+  img {
+    width: rem(165);
+    height: rem(50);
+    /* @include dpr(width, 36px);
       @include dpr(height, 50px); */
-      display: block;
-      margin: 0 auto;
-      padding-top:rem(21);
-     /*  @include dpr(padding-top, 21px); */
-    }
+    display: block;
+    margin: 0 auto;
+    padding-top: rem(21);
+    /*  @include dpr(padding-top, 21px); */
   }
-  .right{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.5);
-    display: none;
-  }
-    .wrong{
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.5);
-    display: none;
-  }
-  .result{
-    position: absolute;
-        top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: white;
-     display: none;
-  }
-  .luckyDraw{
-        position: absolute;
-        top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: none;
-  }
-  .guide{
-            position: absolute;
-        top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.5);
-    display: none;
-  }
-  .draw{
-                position: absolute;
-        top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0,0,0,0.5);
-    /* display: none; */
-  }
+}
+.right {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: none;
+}
+.wrong {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: none;
+}
+.result {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: white;
+  display: none;
+}
+.luckyDraw {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: none;
+}
+.guide {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: none;
+}
 </style>
--- football-quiz/src/components/draw.vue
+++ football-quiz/src/components/draw.vue
@@ -19,14 +19,53 @@ export default {
     return {
       oUl: {},
       speed: 0,
-      timer: 0
+      timer: 0,
+      map: {
+        "20元优惠券": 0,
+        "30元优惠券": 1,
+        "50元优惠券": 2,
+        "全国通兑电影票": 3
+      },
+      index: 0,
+      /* 用于计算经过奖品多少次 */
+      count: 0
     };
   },
+
   mounted() {
     this.__init();
   },
   methods: {
     __init() {
+      var vm = this;
+      //请求答题数据
+      /* !! */
+      /* this.$http
+        .get("https://www.ipareto.com/zeissSjb/draw", {
+          params: {
+            openid: "oYkYa03gpCorQzWW3GCM-uw10aCo",
+            answerId: vm.$route.params.id
+          }
+        })
+        .then(function(response) {
+          response.data.msg = "ok";
+          if (response.data.msg == "ok") {
+              console.log("抽奖结果"+response.data)
+
+          } else {
+            alert("请求问题数据错误");
+          }
+        })
+        .catch(function(response) {
+          //错误处理 比如出现一个蒙层显示网络错误
+          console.log(response);
+ 
+        }); */
+      var data = "20元优惠券";
+      var index = vm.map[data];
+      vm.index = index;
+      console.log("this.$route.params.paicheNo", this.$route.params.id);
+      //轮播初始化
       var vm = this;
       //轮播图部分
       var oUl = vm.$("#div1 ul")[0];
@@ -38,12 +77,12 @@ export default {
       oUl.style.width = oLi[0].offsetWidth * oLi.length + "px";
       vm.oUl = oUl;
       //移动速度
-      var speed = 1;
+      var speed = 3;
       vm.speed = speed;
       //鼠标移开
       //定时器  控制移动的时间
       var timer = requestAnimationFrame(vm.move, 30);
-        vm.timer = timer;
+      vm.timer = timer;
       /*   var timer2=setInterval(index,1000);*/
     },
     move() {
@@ -53,6 +92,8 @@ export default {
       //滑完了一组图片就从头开始
       /* console.log(oUl.offsetLeft+"---"+oUl.offsetWidth/2);*/
       if (oUl.offsetLeft < -oUl.offsetWidth / 2) {
+          /* 转两次就跑到奖品 */
+          vm.count++;
         oUl.style.left = "0";
       }
       if (oUl.offsetLeft > 0) {
@@ -61,36 +102,56 @@ export default {
       oUl.style.left = oUl.offsetLeft - speed + "px";
       /* console.log('oUl.offsetLeft',oUl.offsetLeft);
             console.log('left',oUl.style.left) */
-      vm.timer = requestAnimationFrame(vm.move);
+      /* 计算是否经过奖品 */
+      var oLi = vm.$("#div1 ul li");
+      var index1 = vm.index * oLi[0].offsetWidth;
+      var index2 = (vm.index + 1) * oLi[0].offsetWidth;
+      console.log('oUl.offsetLeft',oUl.offsetLeft)
+      if (oUl.offsetLeft < -index1 && oUl.offsetLeft > -index2) {
+         
+      } else {
+      }
+      if (vm.count < 2) {
+        //可以先执行四次
+        vm.timer = requestAnimationFrame(vm.move);
+      } else {
+          oUl.style.left = index1;
+      }
     }
   }
 };
 </script>
 <style scoped lang="scss" >
 .draw {
-  #m-top{
-          position: absolute;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #3f51b5;
+
+  #m-top {
+    position: absolute;
     top: 40%;
     left: 31%;
-    #div1{
-        position: relative;
- 
-   
-    margin: 0 auto;
-    overflow: hidden;
-     @include dpr(width, 152px);
-    @include dpr(height, 72px);
-    ul {
-      position: absolute;
-      left: 0;
-      top: 0;
-      li {
-        float: left;
-        list-style: none;
+    #div1 {
+      position: relative;
+
+      margin: 0 auto;
+      overflow: hidden;
+      @include dpr(width, 152px);
+      @include dpr(height, 72px);
+      ul {
+        position: absolute;
+        left: 0;
+        top: 0;
+        li {
+          float: left;
+          list-style: none;
+        }
+        @include dpr(width, 1216px);
+        @include dpr(height, 70px);
       }
-      @include dpr(width, 1216px);
-      @include dpr(height, 70px);
-    }
     }
   }
 }
